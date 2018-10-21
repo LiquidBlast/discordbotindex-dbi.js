ec51a2793bfb66d5edb4cd6a6e084e31e9ba299e
Dave R <davetheprogrammer2@gmail.com>
2018-10-21 12:36:53 +0100

    improve it slightly

--- examples/getStats.js
+++ examples/getStats.js
@@ -1,2 +1,2 @@
-const { getStats } = require('../');              // Declaring dbi.js
-getStats('477942073073336320').then(console.log); // Get a bot's stats and log them
+const { getStats } = require('../');                              // Declaring dbi.js
+getStats('477942073073336320').then(stats => console.log(stats)); // Get a bot's stats and log them
